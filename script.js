556b1186c911b55f9ad52bc8bbaa4a9ccc777b27
Flaviojr-dev <flaviobarbosabsjt@gmail.com>
2025-04-28 16:42:41 -0300

    Entrega 2

--- script.js
+++ script.js
@@ -12,3 +12,38 @@ document.addEventListener('click', (e) => {
     dropdownMenu.style.display = 'none';
   }
 });
+
+const especialidades = [
+  "Clínico Geral",
+  "Pediatria",
+  "Ginecologia e Obstetrícia",
+  "Enfermagem",
+  "Odontologia",
+  "Fisioterapia",
+  "Nutrição",
+  "Psicologia",
+  "Assistência Social",
+  "Fonoaudiologia"
+];
+
+const select = document.getElementById("especialidades");
+
+const optionDefault = document.createElement("option");
+optionDefault.value = "";
+optionDefault.textContent = "Todas as especialidades";
+select.appendChild(optionDefault);
+
+especialidades.forEach(especialidade => {
+  const option = document.createElement("option");
+  option.textContent = especialidade;
+  select.appendChild(option);
+});
+
+function procurar() {
+  const endereco = document.getElementById('endereco').value;
+  if (endereco.trim() === "") {
+      alert("Por favor, digite um endereço.");
+  } else {
+      alert(`Buscando postos de saúde próximos a: ${endereco}`);
+  }
+}
